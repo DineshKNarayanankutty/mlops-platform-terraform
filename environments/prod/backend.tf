08d2d3f80cb5eeb7b7c478b4981186d871b858b7
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-02-17 15:20:44 +0530

    Fix S3 backend configuration for production environment

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "mlops-tf-state-prod1"
-    key            = "prod/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket       = "mlops-tf-state-prod"
+    key          = "prod/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
