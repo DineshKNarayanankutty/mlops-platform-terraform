1d66b5c0083b03c7ebbb394c749e3f778b7e5a54
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-02-17 14:51:53 +0530

    Add EKS module configuration for production environment

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,3 +1,11 @@
 module "vpc" {
   source = "../../modules/vpc"
 }
+
+module "eks" {
+  source = "../../modules/eks"
+
+  cluster_name    = "mlops-eks-prod"
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+}
