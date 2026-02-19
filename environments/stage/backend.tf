49ba77ce1695f0c48f36e22b358ee31356dc9167
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-02-19 14:45:13 +0530

    Add Terraform configuration for stage environment

---
+++ environments/stage/backend.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  backend "s3" {
+    bucket       = "mlops-tf-state-prod1"
+    key          = "stage/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.100"
+    }
+  }
+}
---
+++ environments/stage/main.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+  source = "../../modules/vpc"
+}
+
+module "eks" {
+  source = "../../modules/eks"
+
+  cluster_name    = var.cluster_name
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+}
