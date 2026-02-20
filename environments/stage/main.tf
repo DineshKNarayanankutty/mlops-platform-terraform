8a3239a82e2fb2c38eaa99502b1987858d01d33b
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-02-20 20:20:57 +0530

    Refactor EKS module configuration and add S3, ECR modules

--- environments/stage/main.tf
+++ environments/stage/main.tf
@@ -5,7 +5,19 @@ module "vpc" {
 module "eks" {
   source = "../../modules/eks"
 
-  cluster_name    = var.cluster_name
-  vpc_id          = module.vpc.vpc_id
-  private_subnets = module.vpc.private_subnets
+  cluster_name        = var.cluster_name
+  vpc_id              = module.vpc.vpc_id
+  private_subnets     = module.vpc.private_subnets
+  node_instance_type  = "t3.medium"
+}
+
+module "ecr" {
+  source = "../../modules/ecr"
+
+  repository_name = "mlops-stage-inference"
+}
+
+module "s3" {
+  source      = "../../modules/s3"
+  bucket_name = "mlops-stage-artifacts-bucket777"
 }
