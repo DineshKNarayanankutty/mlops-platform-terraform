df3bfe2bfd34a5c1052edcacaea2cb68734f8fc2
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-02-20 22:17:21 +0530

    Refactor VPC module configuration in main.tf to include environment and CIDR variables

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,5 +1,9 @@
 module "vpc" {
-  source = "../../modules/vpc"
+  source               = "../../modules/vpc"
+  environment          = var.environment
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
 }
 
 module "eks" {
@@ -8,4 +12,20 @@ module "eks" {
   cluster_name    = var.cluster_name
   vpc_id          = module.vpc.vpc_id
   private_subnets = module.vpc.private_subnets
+
+  node_instance_type = "t3.large"
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+}
+
+module "ecr" {
+  source = "../../modules/ecr"
+
+  repository_name = "mlops-prod-inference"
+}
+
+module "s3" {
+  source      = "../../modules/s3"
+  bucket_name = "mlops-prod-artifacts-bucket777"
 }
