507bb3c9fab2573dfe540dc0bf6247a7667a7a20
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-02-20 20:21:15 +0530

    Define types for region, environment, and cluster_name variables in variables.tf

--- environments/stage/variables.tf
+++ environments/stage/variables.tf
@@ -1,3 +1,11 @@
-variable "region" {}
-variable "environment" {}
-variable "cluster_name" {}
+variable "region" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
