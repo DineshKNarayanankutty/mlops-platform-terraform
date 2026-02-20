a17387263e46db054bfed5fb3022fea666ca8711
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-02-20 22:18:27 +0530

    Define additional variables for VPC CIDR and subnet CIDRs in variables.tf

--- environments/stage/variables.tf
+++ environments/stage/variables.tf
@@ -3,7 +3,29 @@ variable "region" {
 }
 
 variable "environment" {
-  type = string
+  type    = string
+  default = "stage"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.1.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+  default = [
+    "10.1.1.0/24",
+    "10.1.2.0/24"
+  ]
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+  default = [
+    "10.1.101.0/24",
+    "10.1.102.0/24"
+  ]
 }
 
 variable "cluster_name" {
