0932170797e5faaabbcfe5397166654665314936
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-02-20 22:17:31 +0530

    Define variables for region, environment, cluster name, VPC CIDR, and subnet CIDRs in variables.tf

--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -1,3 +1,35 @@
-variable "region" {}
-variable "environment" {}
-variable "cluster_name" {}
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "environment" {
+  type    = string
+  default = "prod"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "mlops-prod-cluster"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.2.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+  default = [
+    "10.2.1.0/24",
+    "10.2.2.0/24"
+  ]
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+  default = [
+    "10.2.101.0/24",
+    "10.2.102.0/24"
+  ]
+}
